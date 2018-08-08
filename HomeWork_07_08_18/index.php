2ed60f14399ea8f6a1f37816573b74b823059fe9
RustedBird <vlasov.31189@gmail.com>
2018-08-08 11:31:34 +0300

    HomeWork 07_08_18

---
+++ HomeWork_07_08_18/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lesson9</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <form class="jumbotron" id="myForm" action="process.php" method="post">
+        <input class="my-2 px-2 col-md-4 offset-md-4" type="text" name="name" placeholder="Name" required>
+        <input class="my-2 px-2 col-md-4 offset-md-4" type="text" name="age" placeholder="age" required>
+        <select class="my-2 px-2 col-md-4 offset-md-4" name="university_id">
+            <option value="1">DGMA</option>
+            <option value="2">DITM</option>
+            <option value="3">SLAVPED</option>
+            <option value="4">OXFORD</option>
+        </select>
+        <input class="my-2 px-2 col-md-2 offset-md-5" name="send" type="submit" value="send">
+        <h5 class="result text-center"></h5>
+    </form>
+</div>
+<script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+<script>
+    $('#myForm').on('submit', function (event) {
+        event.preventDefault();
+        var $this = $(this),
+            $name = $('input[name="name"]').val(),
+            $age = $('input[name="age"]').val(),
+            $result = $('.result');
+
+        //Проверка имени через регулярное выражение
+        var nameCheck = /[a-zа-яё]{2,15}$/.test($name);
+
+        if (!nameCheck) {
+            $result.html('Имя введено неверно'); //Если проверка не прошла, выводим ошибку
+        } else if (!/^[0-9]*$/.test($age)) { //Проверяем, чтобы введены только цифры
+            $result.html('возраст должен содержать только цифры');
+        } else if ($age < 18 || $age > 100) { //Границы возраста
+            $result.html('возраст должен быть от 18 до 100');
+        } else { //Если все путем, отправляем на запрос
+            $.ajax({
+                url: $this.attr('action'),
+                method: $this.attr('method'),
+                data: $this.serialize(),
+                dataType: 'json',
+                beforeSend: function () {
+
+                },
+                success: function (response) {
+                    $result.html(response.message);
+                },
+                error: function (response) {
+                    $result.html('Unexpected error, please try again');
+                    console.log(response);
+                }
+            });
+        }
+    });
+</script>
+</body>
+</html>
---
+++ HomeWork_07_08_18/process.php
@@ -0,0 +1,38 @@
+<?
+include 'components/db.php';
+
+//Получаю данные формы
+$name = $_POST['name'];
+$age = $_POST['age'];
+$university_id = $_POST['university_id'];
+
+//Задаю начальные положительные значения нашему ответу
+$response['result'] = true;
+$response['message'] = 'Successfully added';
+
+if (mb_strlen($name, 'UTF-8') >= 15) { //Проверка имени
+    $response['result'] = false;
+    $response['message'] = 'name is to long';
+
+} else if ($age < '18' || $age > '100') { //Проверка возраста
+    $response['result'] = false;
+    $response['message'] = 'age is incorrect';
+}
+
+if ($response['result']) { //Если все ОК, соединяемся с базой и пишем туда нашего студента
+    $dbConnection = getConnection();
+
+    $sth = $dbConnection->prepare('INSERT INTO student
+    (name, age, university_id)
+     VALUES (:name, :age, :university_id)');
+
+    $result = $sth->execute([
+            'name' => $name,
+            'age' => $age,
+            'university_id' => $university_id,
+        ]
+    );
+}
+
+echo json_encode($response);
+?>
